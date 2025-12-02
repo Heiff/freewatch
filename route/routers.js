4664f7efc977729d28c4354e5d7cedf934af2d8c
Boho Ikramov <boho4121boho@gmail.com>
2025-12-03 02:23:20 +0300

    succes

--- route/routers.js
+++ route/routers.js
@@ -3,10 +3,10 @@ const { GetAll, GetOne, Delete, Update, ByPage } = require("../controller/Movie.
 const router = Router();
 
 
-router.get("/film",GetAll);
-router.get("/film/:id",GetOne);
-router.delete("/film/:id",Delete);
-router.put("/film/:id",Update);
-router.get("/movies",ByPage);
+router.get("/api/film",GetAll);
+router.get("/api/film/:id",GetOne);
+router.delete("/api/film/:id",Delete);
+router.put("/api/film/:id",Update);
+router.get("/api/movies",ByPage);
 
 module.exports = router;
