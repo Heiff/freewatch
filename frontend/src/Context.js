40acdedb27f4e72f8b758530dc67ce9ab83165f0
Boho Ikramov <boho4121boho@gmail.com>
2025-12-03 02:26:59 +0300

    succes

--- frontend/src/Context.js
+++ frontend/src/Context.js
@@ -7,7 +7,7 @@ import "aos/dist/aos.css";
 
 const Context = createContext()
 const ContextProvider = ({ children }) => {
-  const api = 'http://13.60.191.29'
+  const api = 'http://13.60.191.29/api'
   const navigate = useNavigate();
   const [data,setData] = useState([]);
   const [sortingData,setSortingData] = useState([])
