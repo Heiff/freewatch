54796fe06169d1e9a7dbb246bd9d3b98ec679162
Boho Ikramov <boho4121boho@gmail.com>
2025-12-03 02:31:59 +0300

    succes

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,5 +1,5 @@
 const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize("postgres://postgres:4121@localhost:5433/tgfilm");
+const sequelize = new Sequelize("postgres://postgres:4121@localhost:5432/tgfilm");
 
 module.exports = sequelize;
