07304d4d2e2d085b752544d80d168b77483fa4b7
Boho Ikramov <boho4121boho@gmail.com>
2025-12-15 22:40:47 +0300

    succes

--- app.js
+++ app.js
@@ -87,7 +87,11 @@ bot.on("callback_query", async (query) => {
   const [action, value] = query.data.split(":");
 
   try {
-    const movies = await Movie.findAll({ order: [['film', 'DESC']] });
+    const movies = await Movie.findAll({
+  order: [
+    [Sequelize.literal('film COLLATE "C"'), 'DESC'],
+  ],
+});
 
     if (action === "page") {
       sendMoviePage(chatId, movies, parseInt(value), messageId);
