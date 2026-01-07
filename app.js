534a4a97863d07e94676a6ab62b38409165d61ba
Boho Ikramov <boho4121boho@gmail.com>
2026-01-08 02:26:05 +0300

    succes

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const app = express();
 app.use(cors({ origin: "*", methods: ["GET","POST","PUT","DELETE","OPTIONS"], allowedHeaders: ["Content-Type","Authorization"] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(compression());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'), {
   maxAge: '30d'
 }));
