54db40454c0a3d43d1f956bf7397077e5fc96a15
Boho Ikramov <boho4121boho@gmail.com>
2026-01-08 01:21:24 +0300

    secces

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
-import React from 'react'
+import React,{ Suspense, lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import Movies from './components/Movies'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Movie from './components/Movie'
 import ScrollToTop from './components/ScrollToTop'
-
+const Home = lazy(() => import('./components/Home'));
+const Movies = lazy(() => import('./components/Movies'));
+const Movie = lazy(() => import('./components/Movie'));
 const App = () => {
   return (
     <div>
       <Navbar/>
       <ScrollToTop />
+      <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/movies' element={<Movies/>}/>
         <Route path='/movie/:id' element={<Movie/>}/>
       </Routes>
+      </Suspense>
       <Footer/>
     </div>
   )
