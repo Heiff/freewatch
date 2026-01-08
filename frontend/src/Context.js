5fd436420227690c60b659d7dc5cb3114817fc77
Boho Ikramov <boho4121boho@gmail.com>
2026-01-08 23:29:51 +0300

    succes

--- frontend/src/Context.js
+++ frontend/src/Context.js
@@ -1,8 +1,8 @@
-import React, { createContext, useState, useEffect,lazy } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
+import AOS from "aos";
 import "aos/dist/aos.css";
-const AOS = lazy(() => import('aos'));
 
 
 const Context = createContext()
